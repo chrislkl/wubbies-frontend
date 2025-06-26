4a75e4d757cb1e22b199f8f15e529d0c4df19a0c
chrislkl <christopher.lew916@gmail.com>
2025-06-26 17:43:58 -0400

    updated backend link

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ export default function App() {
 
   const API_BASE = import.meta.env.DEV
   ? "http://localhost:3000"
-  : "https://wubbies-backend.onrender.com";
+  : "wubbies-backend-production.up.railway.app";
 
   async function loadWallet() {
     const token = await getToken()
